082e689c14946400504192e7130a4919896f0466
Jaime Duque <jaimeduque17@gmail.com>
2019-11-13 21:09:48 -0500

    The header and footer are created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import Products from './components/Products';
 import EditProduct from './components/EditProduct';
 import AddProduct from './components/AddProduct';
@@ -8,12 +9,16 @@ import Product from './components/Product';
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route exact path="/products/new" component={AddProduct} />
-				<Route exact path="/products" component={Products} />
-				<Route exact path="/products/:id" component={Product} />
-				<Route exact path="/products/edit/:id" component={EditProduct} />
-			</Switch>
+			<Header />
+			<main className="container mt-5">
+				<Switch>
+					<Route exact path="/new-product" component={AddProduct} />
+					<Route exact path="/products" component={Products} />
+					<Route exact path="/products/:id" component={Product} />
+					<Route exact path="/products/edit/:id" component={EditProduct} />
+				</Switch>
+			</main>
+			<p className="mt-4 p2 text-center">All rights reserved</p>
 		</Router>
 	);
 }
