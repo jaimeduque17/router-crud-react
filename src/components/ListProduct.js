e43d9c601911134c81d1fb1c151b8574b0b5b78d
Jaime Duque <jaimeduque17@gmail.com>
2019-11-13 22:40:50 -0500

    Products shown

--- src/components/ListProduct.js
+++ src/components/ListProduct.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function ListProduct() {
+function ListProduct({ product }) {
+
+    const deleteProduct = id => {
+        console.log('delete', id);
+        // TODO: Delete records
+    }
+
     return (
-        <div></div>
+        <li data-category={product.category} className="list-group-item d-flex justify-content-between align-items-center">
+            <p>
+                {product.saucerName}
+                <span className="font-weight-bold">
+                    {` $${product.saucerPrice}`}
+                </span>
+            </p>
+            <div>
+                <Link
+                to={`/products/edit/${product.id}`}
+                className="btn btn-success mr-2">
+                    Edit
+                </Link>
+                <button
+                type="button"
+                className="btn btn-danger"
+                onClick={() => deleteProduct(product.id)}
+                // &times; entity adds an X next to the text
+                >Delete &times;</button>
+            </div>
+        </li>
     )
 }
 
