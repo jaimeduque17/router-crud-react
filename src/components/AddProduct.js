036918811ce42ea4d64f226bc5945a3fc967571a
Jaime Duque <jaimeduque17@gmail.com>
2019-11-13 23:03:19 -0500

    A form for new products is created

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,8 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddProduct() {
+
+    // state
+    const [saucerName, saveName] = useState('');
+    const [saucerPrice, savePrice] = useState('');
+    const [category, saveCategory] = useState('');
+
+    const readRadioValue = e => {
+        saveCategory(e.target.value);
+    }
+
     return (
-        <h1 className="text-center">Add New Product</h1>
+        <div className="col-md-8 mx-auto">
+            <h1 className="text-center">Add New Product</h1>
+            <form
+                className="mt-5"
+            >
+                <div className="form-group">
+                    <label>Saucer Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        placeholder="Saucer Name"
+                        onChange={e => saveName(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Saucer Price</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="price"
+                        placeholder="Saucer Price"
+                        onChange={e => savePrice(e.target.value)}
+                    />
+                </div>
+                <legend className="text-center">Category:</legend>
+                <div className="text-center">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="dessert"
+                            onChange={readRadioValue}
+                        />
+                        <label className="form-check-label">
+                            Dessert
+                    </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="drink"
+                            onChange={readRadioValue}
+                        />
+                        <label className="form-check-label">
+                            Drink
+                    </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="cuts"
+                            onChange={readRadioValue}
+                        />
+                        <label className="form-check-label">
+                            Cuts
+                    </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="salad"
+                            onChange={readRadioValue}
+                        />
+                        <label className="form-check-label">
+                            Salad
+                    </label>
+                    </div>
+                </div>
+                <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Add Product" />
+            </form>
+        </div>
     )
 }
 
